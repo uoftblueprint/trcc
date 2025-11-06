35185eb900af78f03ecaf845a558a8dc37dae792
Jack Le <huuanducle@gmail.com>
2025-11-06 13:13:08 -0500

    added linting checks and autocorrect using eslint and prettier (#10)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { createClient } from '@/utils/supabase/server';
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
   const supabase = await createClient();
   const { data: example } = await supabase.from("example").select();
 
-  return <pre>{JSON.stringify(example, null, 2)}</pre>
+  return <pre>{JSON.stringify(example, null, 2)}</pre>;
 }
