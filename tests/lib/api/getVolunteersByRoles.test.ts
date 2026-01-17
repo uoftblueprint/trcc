4d8b6ed8f4efd03ea61108ebd3a2648e06c41fe3
Michelle <michh2005@gmail.com>
2026-01-17 02:11:30 -0500

    created/modified unit tests and integration tests

--- tests/lib/api/getVolunteersByRoles.test.ts
+++ tests/lib/api/getVolunteersByRoles.test.ts
@@ -1,38 +1,27 @@
-// Example test for getExample function
-// This test is not meaningful as is, but serves as a template
-// You should modify it to fit your actual implementation and testing needs
-
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { getVolunteersByRoles } from "@/lib/api/getVolunteersByRoles";
 import { createClient } from "@/lib/client/supabase/server";
+import { makeTestVolunteerInsert } from "../support/factories";
 
 const Roles1 = [
   {
     Roles: { name: "Role 1" },
-    Volunteers: {
-      id: 1,
-    },
+    Volunteers: makeTestVolunteerInsert({ id: 1 }),
   },
   {
     Roles: { name: "Role 1" },
-    Volunteers: {
-      id: 3,
-    },
+    Volunteers: makeTestVolunteerInsert({ id: 3 }),
   },
 ];
 
 const Roles2 = [
   {
     Roles: { name: "Role 2" },
-    Volunteers: {
-      id: 2,
-    },
+    Volunteers: makeTestVolunteerInsert({ id: 2 }),
   },
   {
     Roles: { name: "Role 2" },
-    Volunteers: {
-      id: 3,
-    },
+    Volunteers: makeTestVolunteerInsert({ id: 3 }),
   },
 ];
 
@@ -53,30 +42,61 @@ describe("getVolunteersByRoles", () => {
     vi.mocked(createClient).mockResolvedValue(mockClient);
   });
 
+  it("returns no volunteers given empty array of filters", async () => {
+    mockIn.mockResolvedValueOnce({ data: [], error: null });
+
+    const result = await getVolunteersByRoles("OR", ["Role 1", "Role 2"]);
+    expect(result.status).toBe(200);
+    expect(result).toHaveProperty("data");
+    expect(result.data).toHaveLength(0);
+  });
+
   it("returns error response for an invalid operator", async () => {
-    const result = await getVolunteersByRoles("INVALID", ["Role 1"]);
-    expect(result.status).toBe(400);
-    expect(result).toEqual({ status: 400, error: "Operator is not AND or OR" });
+    const { data, error, status } = await getVolunteersByRoles("INVALID", [
+      "Role 1",
+    ]);
+    expect(data).toBeNull();
+    expect(status).toBe(400);
+    expect(error).toEqual("Operator is not AND or OR");
   });
 
   it("returns error response if the filters array is malformed", async () => {
-    const result = await getVolunteersByRoles("OR", [1]);
-    expect(result.status).toBe(400);
-    expect(result).toEqual({
-      status: 400,
-      error: "Roles to filter by are not all strings",
-    });
+    const { data, error, status } = await getVolunteersByRoles("OR", [1]);
+    expect(data).toBeNull();
+    expect(status).toBe(400);
+    expect(error).toEqual("Roles to filter by are not all strings");
   });
 
-  it("returns all volunteers with Role 1 or Role 2", async () => {
+  it("returns all volunteers with Role 1 OR Role 2", async () => {
     mockIn.mockResolvedValueOnce({ data: [...Roles1, ...Roles2], error: null });
 
     const result = await getVolunteersByRoles("OR", ["Role 1", "Role 2"]);
     expect(result.status).toBe(200);
     expect(result).toHaveProperty("data");
 
-    const ids = result.data?.map((volunteer) => volunteer.id).sort();
-    expect(ids).toEqual([1, 2, 3]);
+    const rolesByVolunteerId = new Map(
+      result.data?.map((volunteer) => [volunteer.id, volunteer.filtered_roles])
+    );
+
+    expect([...rolesByVolunteerId.keys()]).toHaveLength(3);
+    expect([...rolesByVolunteerId.keys()]).toEqual(
+      expect.arrayContaining([1, 2, 3])
+    );
+
+    expect(rolesByVolunteerId.get(1)).toHaveLength(1);
+    expect(rolesByVolunteerId.get(1)).toEqual(
+      expect.arrayContaining(["Role 1"])
+    );
+
+    expect(rolesByVolunteerId.get(2)).toHaveLength(1);
+    expect(rolesByVolunteerId.get(2)).toEqual(
+      expect.arrayContaining(["Role 2"])
+    );
+
+    expect(rolesByVolunteerId.get(3)).toHaveLength(2);
+    expect(rolesByVolunteerId.get(3)).toEqual(
+      expect.arrayContaining(["Role 1", "Role 2"])
+    );
   });
 
   it("returns all volunteers with Role 1", async () => {
@@ -86,8 +106,24 @@ describe("getVolunteersByRoles", () => {
     expect(result.status).toBe(200);
     expect(result).toHaveProperty("data");
 
-    const ids = result.data?.map((volunteer) => volunteer.id).sort();
-    expect(ids).toEqual([1, 3]);
+    const rolesByVolunteerId = new Map(
+      result.data?.map((volunteer) => [volunteer.id, volunteer.filtered_roles])
+    );
+
+    expect([...rolesByVolunteerId.keys()]).toHaveLength(2);
+    expect([...rolesByVolunteerId.keys()]).toEqual(
+      expect.arrayContaining([1, 3])
+    );
+
+    expect(rolesByVolunteerId.get(1)).toHaveLength(1);
+    expect(rolesByVolunteerId.get(1)).toEqual(
+      expect.arrayContaining(["Role 1"])
+    );
+
+    expect(rolesByVolunteerId.get(3)).toHaveLength(1);
+    expect(rolesByVolunteerId.get(3)).toEqual(
+      expect.arrayContaining(["Role 1"])
+    );
   });
 
   it("returns all volunteers with Role 1 AND Role2", async () => {
@@ -97,7 +133,15 @@ describe("getVolunteersByRoles", () => {
     expect(result.status).toBe(200);
     expect(result).toHaveProperty("data");
 
-    const ids = result.data?.map((volunteer) => volunteer.id).sort();
-    expect(ids).toEqual([3]);
+    const rolesByVolunteerId = new Map(
+      result.data?.map((volunteer) => [volunteer.id, volunteer.filtered_roles])
+    );
+
+    expect([...rolesByVolunteerId.keys()]).toEqual([3]);
+
+    expect(rolesByVolunteerId.get(3)).toHaveLength(2);
+    expect(rolesByVolunteerId.get(3)).toEqual(
+      expect.arrayContaining(["Role 1", "Role 2"])
+    );
   });
 });
