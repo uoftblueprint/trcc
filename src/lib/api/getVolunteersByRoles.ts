f6f65d513146a005a2fd52118200e810d11f6e9b
Michelle <michh2005@gmail.com>
2026-01-17 14:47:44 -0500

    removed http codes

--- src/lib/api/getVolunteersByRoles.ts
+++ src/lib/api/getVolunteersByRoles.ts
@@ -7,8 +7,8 @@ type VolunteerWithRoles = Volunteer & {
   filtered_roles: string[];
 };
 type GetVolunteersByRolesResult =
-  | { data: VolunteerWithRoles[]; status: 200; error: null }
-  | { data: null; status: 400 | 500; error: string };
+  | { data: VolunteerWithRoles[]; error: null }
+  | { data: null; error: string };
 
 export function isAllStrings(arr: unknown[]): arr is string[] {
   return arr.every((item) => typeof item === "string");
@@ -26,12 +26,11 @@ export async function getVolunteersByRoles(
   filters: string[]
 ): Promise<GetVolunteersByRolesResult> {
   if (!isValidOperator(operator)) {
-    return { status: 400, error: "Operator is not AND or OR", data: null };
+    return { error: "Operator is not AND or OR", data: null };
   }
 
   if (!isAllStrings(filters)) {
     return {
-      status: 400,
       error: "Roles to filter by are not all strings",
       data: null,
     };
@@ -59,7 +58,6 @@ export async function getVolunteersByRoles(
     });
 
     return {
-      status: 500,
       error: `Failed to query Supabase database: ${error.message}`,
       data: null,
     };
@@ -107,7 +105,6 @@ export async function getVolunteersByRoles(
 
   return {
     data: filteredVolunteers,
-    status: 200,
     error: null,
   };
 }
