d5fac62630d2fa393b696944e0d897e727cd1d41
Jonathanqiao1 <jonathanqiao300@gmail.com>
2026-01-24 00:19:15 -0500

    Export getVolunteersTable in API index

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -3,3 +3,4 @@
 export { getExample } from "./getExample";
 export { createRole } from "./createRole";
 export { getVolunteersByCohorts } from "./getVolunteersByCohorts";
+export { getVolunteersTable } from "./getVolunteersTable";
