13841d90c8818de99cb23fd7edda7e45de51c058
Alan Su <alansu1010@gmail.com>
2026-01-29 16:04:44 -0500

    Update removeRole to remove by name and type

--- src/lib/api/removeRole.ts
+++ src/lib/api/removeRole.ts
@@ -1,17 +1,19 @@
 import { createClient } from "../client/supabase/server";
 
 /**
- * Removes a Role and associated VolunteerRole rows by name.
+ * Removes a Role and associated VolunteerRole rows by name and type.
  * Note that this assumes that the deletion to cascade to the VolunteerRoles
  * table.
  * @param roleName - The name of the role to remove.
+ * @param roleType - The type of the role to remove.
  */
 type RemoveRoleResponse =
   | { success: true; error?: never }
   | { success: false; error: string };
 
 export async function removeRole(
-  roleName: string
+  roleName: string,
+  roleType: string
 ): Promise<RemoveRoleResponse> {
   const client = await createClient();
   try {
@@ -19,13 +21,17 @@ export async function removeRole(
       .from("Roles")
       .delete()
       .eq("name", roleName)
+      .eq("type", roleType)
       .select();
 
     if (error) throw error;
 
     // No role with that name was found
     if (data.length === 0)
-      return { success: false, error: `Role with name ${roleName} not found` };
+      return {
+        success: false,
+        error: `Role with name ${roleName} and type ${roleType} not found`,
+      };
 
     return { success: true };
   } catch (err) {
