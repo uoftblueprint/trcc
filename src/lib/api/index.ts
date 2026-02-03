1abc5cabf67a10b83dc2e0e388549083ddf593e2
lehuu2 <jackl32482005@gmail.com>
2026-02-03 17:13:40 -0500

    finished removeVolunteer PR, changed function and branch namings

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,8 +1,8 @@
 // Add any exports from your API files here
 
 export { getExample } from "./getExample";
+export { removeVolunteer } from "./removeVolunteer";
 export { getVolunteersByCohorts } from "./getVolunteersByCohorts";
 export { getVolunteersByMultipleColumns } from "./getVolunteersByMultipleColumns";
 export { getVolunteersByRoles } from "./getVolunteersByRoles";
 export { createVolunteer } from "./createVolunteer";
-export { deleteVolunteer } from "./deleteVolunteer";
