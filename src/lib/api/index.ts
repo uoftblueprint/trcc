92414e0c3a0c67449aac26875a624195021e764a
Dimural Murat <dimural722@gmail.com>
2026-02-13 23:23:35 -0500

    update role pr finished

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -25,4 +25,3 @@ export { updateCohort } from "./updateCohort";
 export { createCohort } from "./createCohort";
 export { getCohorts } from "./getCohorts";
 export { removeCohort } from "./removeCohort";
-export { getUsers } from "./getUsers";
