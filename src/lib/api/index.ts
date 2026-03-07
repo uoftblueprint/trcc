a03c02cc3a3f94e1877caf6f6651cca2d36540d6
lehuu2 <jackl32482005@gmail.com>
2026-03-07 08:26:49 -0500

    fixed some implementations and added testings

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -21,3 +21,4 @@ export { updateRole } from "./updateRole";
 export { updateVolunteer } from "./updateVolunteer";
 export { updateCohort } from "./updateCohort";
 export { createCohort } from "./createCohort";
+export { getCohorts } from "./getCohorts";
